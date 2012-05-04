27a64d8b62b1d837fa8af1f1c3c977bb76b4ac72
wolcano <wolcano@mkaras-sk-eeepc>
2012-05-04 17:50:30 +0200

    recursive traversing of simple classes (INT only), changing argument values

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,12 +1,29 @@
 
 public class HelloWorldApp {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		procedure_1_int(13);
+	static class HelloWorldClass {
+		public static void procedure_1_int(int i1, float f, char c, double d, long l, String s, int i2, Object cl) {
+			System.out.println("i1 is "+i1);
+			System.out.println("f is "+f);
+			System.out.println("c is "+c);
+			System.out.println("d is "+d);
+			System.out.println("l is "+l);
+			System.out.println("s is "+s);
+			System.out.println("i2 is "+i2);
+			System.out.println("cl is "+(cl == null ? "NULL" : cl.getClass()));
+		}
 	}
 
-	public static void procedure_1_int(int i) {
-		System.out.println("i is "+i);
+	static HelloWorldClass hwc;
+
+	void HelloWorldApp() {
+		hwc = new HelloWorldClass();
+		main(null);
 	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		HelloWorldClass.procedure_1_int(0, 1, 'X', 2, 3, "XYZ", 4, hwc);
+	}
+
 }
 
--- Paralen.java
+++ Paralen.java
@@ -19,11 +19,6 @@ import java.security.ProtectionDomain;
 
 public class Paralen implements ClassFileTransformer
 {
-	private static String tgtcls = "HelloWorldApp";
-	//private static String mtd = "<init>";
-	private static String mtd = "";
-	
-
 	public static void premain (String argv, Instrumentation i)
 		throws ClassNotFoundException, InstantiationException, IllegalAccessException
 	{
@@ -34,7 +29,9 @@ public class Paralen implements ClassFileTransformer
 	public byte[] transform (ClassLoader cl, String className, Class classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer)
 	{
 		//System.out.println("loading class "+className);
-		if (!className.equals(tgtcls)) {
+		if (className.indexOf("sun") == 0 ||
+					className.indexOf("java") == 0) // we target only simple classes
+		{
 			return null;
 		}
 		System.out.println("ClassLoader is "+cl);
@@ -56,53 +53,64 @@ public class Paralen implements ClassFileTransformer
 		}
 
 		ConstantPoolGen _cp = _cg.getConstantPool(); //Get the class' constantpool
-		System.out.println("Method getMethods(): "+_cg.getMethods().length);
+		System.out.println("# Method getMethods(): "+_cg.getMethods().length);
 		try {
 			Method[] meths = _cg.getMethods();
 			for(Method m : meths) //Repeat for all methods in the class
 			{
 				if (m.getName().equals("<init>")) {
-					System.out.println("Skipping method "+m.getName());
+					System.out.println("## Skipping method "+m.getName());
 					continue;
 				}
-				if (mtd == "" || m.getName().indexOf(mtd) >= 0)
+				//if (mtd == "" || m.getName().indexOf(mtd) >= 0)
 				{
-					System.out.println("Found method: "+m.toString());
+					System.out.println("## Processing method: "+m.toString());
 					try {
-						System.out.println("dumping argument types");
+						System.out.println("### Dumping argument types");
 
 						MethodGen methodGen = new MethodGen(m, _cg.getClassName(), _cp); //Create a new MethodGen (the equivilent of createing a brand new method
 						InstructionList iList = methodGen.getInstructionList(); //Create a new list of instructions for our method
 						InstructionFactory _factory = new InstructionFactory(_cg, _cp);
 
 						Type[] ts = m.getArgumentTypes();
-						System.out.println("phase 2. argument count is "+ts.length);
-						int pos = 1; // zacneme od 1. argumentu, 0 je this
+						System.out.println("#### argument count is "+ts.length);
+						int pos = 0; // zacneme od 1. argumentu, 0 je this
 						for(int i = 0; i < ts.length; i++) {
-							System.out.println("phase 2. i="+i);
 							Type t = ts[i];
-							// we start experiment with INTs
-							if (!t.toString().equals("int")) {
-								System.out.println("skipping argument of type "+t);
-								continue;
-							}
-
-							{
-								InstructionList il = new InstructionList();
-								// print static info about argument
-								InstructionHandle ih_0 = il.append(_factory.createFieldAccess("java.lang.System", "out", new ObjectType("java.io.PrintStream"), Constants.GETSTATIC));
-								il.append(new PUSH(_cp, "Value of argument "+i+"("+t.toString()+"):"));
-								il.append(_factory.createInvoke("java.io.PrintStream", "println", Type.VOID, new Type[] { Type.STRING }, Constants.INVOKEVIRTUAL));
-
-								// now print runtime value
-								InstructionHandle ih_8 = il.append(_factory.createFieldAccess("java.lang.System", "out", new ObjectType("java.io.PrintStream"), Constants.GETSTATIC));
-								il.append(_factory.createLoad(Type.INT, i));
-								il.append(_factory.createInvoke("java.io.PrintStream", "println", Type.VOID, new Type[] { Type.INT }, Constants.INVOKEVIRTUAL));
-
+							// two positions for LONG and DOUBLE
+							if (t == Type.LONG || t == Type.DOUBLE) {
+								System.out.println("#### long argument ++");
 								pos++;
-
-								iList.insert(il);
 							}
+
+							// we start experiment with INTs
+							System.out.println("#### argument "+i+"("+pos+") of type "+t);
+							if (!t.toString().equals("int")) {
+								System.out.println("##### skipped");
+							}
+							else {
+								//System.out.println("#### processing...");
+								{
+									InstructionList il = new InstructionList();
+									// print static info about argument
+									InstructionHandle ih_0 = il.append(_factory.createFieldAccess("java.lang.System", "out", new ObjectType("java.io.PrintStream"), Constants.GETSTATIC));
+									il.append(new PUSH(_cp, "Original value of input argument "+i+"("+t.toString()+"):"));
+									il.append(_factory.createInvoke("java.io.PrintStream", "println", Type.VOID, new Type[] { Type.STRING }, Constants.INVOKEVIRTUAL));
+
+									// now print runtime value
+									InstructionHandle ih_8 = il.append(_factory.createFieldAccess("java.lang.System", "out", new ObjectType("java.io.PrintStream"), Constants.GETSTATIC));
+									il.append(_factory.createLoad(Type.INT, pos));
+									il.append(_factory.createInvoke("java.io.PrintStream", "println", Type.VOID, new Type[] { Type.INT }, Constants.INVOKEVIRTUAL));
+
+									// now we replace value with something else
+									il.append(new PUSH(_cp, 7));
+									il.append(_factory.createStore(Type.INT, pos));
+
+									iList.insert(il);
+								}
+							}
+
+							pos++;
 						}
 
 						{
@@ -120,20 +128,20 @@ public class Paralen implements ClassFileTransformer
 						methodGen.removeLineNumbers(); //cleanup
 						_cg.replaceMethod(m, methodGen.getMethod()); //replace the old method with our new one
 
-						System.out.println("Method replacement ok.");
+						System.out.println("### Method replacement ok.");
 					}
 					catch (Exception e) {
-						System.out.println("Method replacement failed.");
+						System.out.println("### Method replacement failed.");
 						System.err.println(Arrays.toString(e.getStackTrace()));
 					}
 				}
 				else {
-					System.out.println("Skipping method "+m.getName()+".");
+					System.out.println("## Skipping method "+m.getName()+".");
 				}
 			}
 		}
 		catch (Exception e) {
-			System.out.println("Method traversal failed.");
+			System.out.println("## Methods traversal failed.");
 			System.err.println(Arrays.toString(e.getStackTrace()));
 		}
 
